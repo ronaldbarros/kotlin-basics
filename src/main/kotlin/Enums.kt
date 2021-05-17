ada34ef3a296c4a55081e59e4cae0b29e1067659
ronaldbarros <rsmbarros@gmail.com>
2021-05-16 22:52:05 -0300

    created enums example

---
+++ src/main/kotlin/Enums.kt
@@ -0,0 +1,14 @@
+enum class Color(val red: Int, val green: Int, val blue: Int){
+    RED(255, 0, 0),
+    ORANGE(255,165,0),
+    YELLOW(255,255,0),
+    GREEN(0,255,0),
+    BLUE(0,0,255);
+
+    fun rgb() = ((red * 256 + green) * 256 + blue) //return decimal value
+}
+
+fun main(){
+    val rgb = Color.RED.rgb()
+    println(rgb)
+}
